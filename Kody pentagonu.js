c80237d7c7928c1e7c998e2dc34998137cab61e5
Zimny <126821624+Zimny32@users.noreply.github.com>
2023-03-02 23:39:30 +0100

    Create Kody pentagonu.js
    You will never understand the hell that is inside my head...

---
+++ Kody pentagonu.js
@@ -0,0 +1,143 @@
+// let sonic = 1;
+// let shadow = 2135;
+// let gami = "gami to furry";
+// console.log("gami to furras ", shadow + sonic);
+// let array = [2135, 2137];
+// for (let index = 0; index < array.length; index += 1) {
+//   const element = array[index];
+//   console.log(element);
+// }
+
+// while (shadow < 2137) {
+//   shadow += 1;
+
+//   console.log(shadow);
+
+//   if (shadow <= 2138) console.log(sonic);
+
+//   switch (shadow) {
+//     case 2137:
+//       console.log("gami to furras");
+//       break;
+
+//     default:
+//       console.log(gami);
+//       break;
+//   }
+// }
+// function fun(heliotrop) {
+//   console.log("gami to furras " + heliotrop);
+//   return gami;
+// }
+// fun(sonic);
+// fun(shadow);
+// let silver = fun("");
+// console.log(silver);
+
+// if (shadow == 2135) {
+//   console.log("agagdfgdfdgd");
+// }
+// let player1 = "papierz";
+// let player2 = "gami to furras";
+
+// if (player1 == "papierz" && player2 == "bocchi the rock") {
+//   console.log("player 1 to mistrz pokemon");
+// } else if (player1 == "bocchi the rock" && player2 == "papierz") {
+//   console.log("player 2 zadzwonił do amity");
+// } else if (player1 == player2) {
+//   console.log("Karzdy w rzyću pżegrywa");
+// } else {
+//   console.log("gami to furras");
+// }
+// function isEven(number) {
+//   if (number % 2 == 0) return true;
+//   return false;
+// }
+// let number = 45;
+// console.log(isEven(number));
+
+//zrób kanapkę
+// 1. weź chleb
+// 2. weź masło
+// 3. weź ser
+// 4. weź szynke
+// 5. weź pomidory
+// 6. weź ogórki
+// 7. weź sałatę
+// 8. weź majonez
+// 9. weź musztardę
+// 10. weź ketchup
+// 11. weź sos sojowy
+// 12. weź sos pomidorowy
+// 13. weź sos czosnkowy
+// 14. weź sos BBQ
+// 15. weź sos słodko-kwaśny
+// 16. weź sos czekoladowy
+// 17. weź sos czekoladowy
+// 18. weź sos czekoladowy
+// 19. weź sos czekoladowy
+// 20. pokrój chleb na pół
+// 21. rozsmaruj masło na chlebie
+// 22. rozsmaruj majonez na chlebie
+// 23. rozsmaruj musztardę na chlebie
+// 24. rozsmaruj ketchup na chlebie
+// 25. rozsmaruj sos sojowy na chlebie
+// 26. rozsmaruj sos pomidorowy na chlebie
+// 27. rozsmaruj sos czosnkowy na chlebie
+// 28. rozsmaruj sos BBQ na chlebie
+// 29. rozsmaruj sos słodko-kwaśny na chlebie
+// 30. rozsmaruj sos czekoladowy na chlebie
+// 31. rozsmaruj sos czekoladowy na chlebie
+// 32. rozsmaruj sos czekoladowy na chlebie
+// 33. rozsmaruj sos czekoladowy na chlebie
+// 34. rozsmaruj sos czekoladowy na chlebie
+// 35. rozsmaruj sos czekoladowy na chlebie
+// 36. zetrzyj ser na chlebie
+// 37. zetrzyj szynke na chlebie
+// 38. zetrzyj pomidory na chlebie
+// 39. zetrzyj ogórki na chlebie
+// 40. posyp sałatą na chlebie
+// 41. zetrzyj ser na drugiej części chleba
+// 42. zetrzyj szynke na drugiej części chleba
+// 43. zetrzyj pomidory na drugiej części chleba
+// 44. zetrzyj ogórki na drugiej części chleba
+// 45. posyp sałatą na drugiej części chleba
+// 46. połącz chleb
+// 47. zjedz kanapkę
+// 48. zjedz kanapkę
+// 49. zjedz kanapkę
+
+// let kanapka = "yummy🤤😝🤤🤤🤤🤤🤤🤤🤤🤤🤤🤤🤤🤤🤤😒😒😓😓😓🤤🤤😒🤤🤤🤤🤤🤤🤤🤤🤤🤤";
+// let SosCzekoladowy = "Questionable";
+// let StartyOgórek = "????????????????????????????????????????????????????????";
+
+// function ohm(I, R) {
+//   return I * R;
+// }
+// let U = ohm(23, 54);
+// console.log(U);
+
+// function PoleTrojkata(a, h) {
+//   return (a * h) / 2;
+// }
+// let Trujkont = PoleTrojkata(5, 7);
+// console.log(Trujkont);
+
+// function PoleKola(r) {
+//   return (r * 2) / Math.PI;
+// }
+// let Couo = PoleKola(8);
+// console.log(Couo);
+let array = [82, 231, 2847, 273, 102, 478, 1237, 2137, 32, 87, 365];
+let lichba = 0;
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+//   lichba += element;
+// }
+// console.log(lichba);
+let index = 0;
+while (index < array.length) {
+  lichba += array[index];
+  index += 1;
+}
+console.log(lichba);
